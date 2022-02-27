15ad50806d3a045609964660ed59a1cbdf1321a0
RTroMT <n.minhtri1105@gmail.com>
2022-02-27 16:10:57 +0700

    Done unmodified

---
+++ contest_1328/problem_A.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 0, b = 0;
+    int t = 0, result = 0;
+    scanf("%d", &t);
+    for(int i1 = 0; i1 < t; i1++)
+    {
+        result = 0;
+        scanf("%d", &a);
+        scanf("%d", &b);
+        if(a % b == 0)
+        {
+            printf("%d\n", result);
+        }
+        else
+        {
+            while(a % b != 0)
+            {
+                a += 1;
+                result += 1;
+            }
+            printf("%d\n", result);
+        }     
+    }   
+    return 0;
+}
